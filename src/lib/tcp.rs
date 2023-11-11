fefe14af1d125c60e630016b49811c51b6169847
paudelacuesta <paudelacuesta@ciclion.com>
2023-11-11 16:18:32 +0100

    chore: No Panics & Override WellKnownPort when specified in URL

--- src/lib/tcp.rs
+++ src/lib/tcp.rs
@@ -1,28 +1,32 @@
 use std::{net::IpAddr, time::Duration};
 use tokio::net::TcpStream;
+use url::Url;
 
 use crate::{error::Result, Error};
 
 const HTTP_PORT: u16 = 80;
 const HTTPS_PORT: u16 = 443;
 
-fn build_addr(ip: IpAddr, scheme: &str) -> Result<String> {
-    match scheme {
-        "http" => Ok(format!("{}:{}", ip, HTTP_PORT)),
-        "https" => Ok(format!("{}:{}", ip, HTTPS_PORT)),
-        _ => Err(Error::TcpSchemeNotSupported),
+fn build_addr(ip: IpAddr, url: &Url) -> Result<String> {
+    match url.port() {
+        Some(port) => Ok(format!("{}:{}", ip, port)),
+        None => match url.scheme() {
+            "http" => Ok(format!("{}:{}", ip, HTTP_PORT)),
+            "https" => Ok(format!("{}:{}", ip, HTTPS_PORT)),
+            _ => Err(Error::TcpSchemeNotSupported),
+        },
     }
 }
 
-pub async fn connect(ip: IpAddr, scheme: &str) -> Result<TcpStream> {
-    let address = build_addr(ip, scheme)?;
+pub async fn connect(ip: IpAddr, url: &Url) -> Result<TcpStream> {
+    let address = build_addr(ip, url)?;
     TcpStream::connect(address)
         .await
         .map_err(|e| Error::TcpConnect(e.to_string()))
 }
 
-pub async fn connect_timeout(ip: IpAddr, scheme: &str, timeout: f32) -> Result<TcpStream> {
-    let address = build_addr(ip, scheme)?;
+pub async fn connect_timeout(ip: IpAddr, url: &Url, timeout: f32) -> Result<TcpStream> {
+    let address = build_addr(ip, url)?;
 
     match tokio::time::timeout(
         Duration::from_secs_f32(timeout),
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ async fn main() {
     // Load Environment Variables from .env file
     dotenv::from_filename(String::from(".env")).ok();
 
+    // Perform cleanup actions when the Ctrl+C signal is received
+    ctrlc::set_handler(move || {
+        // Cleanup
+        println!("Graceful shutdown");
+        // Terminate the program after cleanup
+        std::process::exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     // Parse Input Parameters
     let params = rucl::Params::parse();
     // Init Tracing
@@ -23,80 +32,110 @@ async fn main() {
     let host = params
         .url
         .host()
-        .unwrap_or_else(|| panic!("ParseParams: Missing Hostname"))
+        .unwrap_or_else(|| {
+            println!("FATAL ParseHost");
+            std::process::exit(0);
+        })
         .to_string();
     log::warn!("Connecting to {}", &host.blue().bold());
     let ip = match rucl::dns::query(host.clone()).await {
         Ok(i) => i,
-        Err(e) => panic!("QueryDNS: {:?}", e),
+        Err(e) => {
+            println!("FATAL QueryDns: {:?}", e);
+            std::process::exit(0);
+        }
     };
     log::warn!("{} {}", "DNS OK:".green(), ip.to_string().blue().bold());
 
     // Establish TCP Connection to specific port depending on URL scheme
     let mut tcp = match params.connect_timeout {
-        Some(t) => rucl::tcp::connect_timeout(ip, params.url.scheme(), t)
+        Some(t) => rucl::tcp::connect_timeout(ip, &params.url, t)
             .await
-            .unwrap_or_else(|e| panic!("EstablishTCPTimeout: {:?}", e)),
-        None => rucl::tcp::connect(ip, params.url.scheme())
+            .unwrap_or_else(|e| {
+                println!("FATAL EstablishTcpTimeout: {:?}", e);
+                std::process::exit(0);
+            }),
+        None => rucl::tcp::connect(ip, &params.url)
             .await
-            .unwrap_or_else(|e| panic!("EstablishTCP: {:?}", e)),
+            .unwrap_or_else(|e| {
+                println!("FATAL EstablishTcp: {:?}", e);
+                std::process::exit(0);
+            }),
     };
     log::info!("{}", "Establish TCP OK".to_string().green());
 
     let http_request = rucl::http::build_request(params.clone())
         .await
-        .unwrap_or_else(|e| panic!("HttpReq: {}", e));
+        .unwrap_or_else(|e| {
+            println!("FATAL BuildHttpRequest: {:?}", e);
+            std::process::exit(0);
+        });
     log::info!("{}:\n{}", "HttpReq OK".green(), &http_request.blue());
 
     if params.url.scheme() == "https" {
         // Start TLS Session
         let mut tls = rucl::tls::start_session(host, tcp)
             .await
-            .unwrap_or_else(|e| panic!("StartTLS: {:?}", e));
+            .unwrap_or_else(|e| {
+                println!("FATAL StartTls: {:?}", e);
+                std::process::exit(0);
+            });
         log::info!("{}", "Start TLS OK".to_string().green());
 
         // Send HTTP Request
         tls.write_all(http_request.as_bytes())
             .await
-            .unwrap_or_else(|e| panic!("TxTLS: {:?}", e));
+            .unwrap_or_else(|e| {
+                println!("FATAL TxTls: {:?}", e);
+                std::process::exit(0);
+            });
         log::info!("{}", "Tx TLS OK".to_string().green());
 
         // Parse HTTP Response
         let mut res = vec![];
-        let read_bytes = tls
-            .read_to_end(&mut res)
-            .await
-            .unwrap_or_else(|e| panic!("RxTLS: {:?}", e));
+        let read_bytes = tls.read_to_end(&mut res).await.unwrap_or_else(|e| {
+            println!("FATAL RxTls: {:?}", e);
+            std::process::exit(0);
+        });
         log::info!("{}", "Rx TLS OK".to_string().green());
         let read_string = format!("Read {} bytes", read_bytes);
         log::warn!("{}: {}", "TLS OK".green(), read_string.blue().bold());
 
         // Drop TLS connection
-        tls.shutdown()
-            .await
-            .unwrap_or_else(|e| panic!("DropTLS: {:?}", e));
+        tls.shutdown().await.unwrap_or_else(|e| {
+            println!("FATAL DropTls: {:?}", e);
+            std::process::exit(0);
+        });
 
         // Display/Store Results
         println!("{}", String::from_utf8_lossy(&res));
     } else {
         // Send HTTP Request
-        tcp.write_all(http_request.as_bytes()).await.unwrap();
+        tcp.write_all(http_request.as_bytes())
+            .await
+            .unwrap_or_else(|e| {
+                println!("FATAL WriteTcp {:?}", e);
+                std::process::exit(0);
+            });
         log::info!("{}", "Tx TCP OK".green());
 
         // Parse HTTP Response
         let mut res = vec![];
-        let read_bytes = match tcp.read_to_end(&mut res).await {
-            Ok(b) => b,
-            Err(e) => panic!("ReadTCP{:?}", e),
-        };
+        let read_bytes = tcp.read_to_end(&mut res)
+            .await
+            .unwrap_or_else(|e| {
+                println!("FATAL ReadTcp {:?}", e);
+                std::process::exit(0);
+            });
         log::info!("{}", "Rx TCP OK".green());
         let read_string = format!("Read {} bytes", read_bytes);
         log::warn!("{} {}", "TCP OK:".green(), read_string.blue().bold());
 
         // Drop TCP connection
-        tcp.shutdown()
-            .await
-            .unwrap_or_else(|e| panic!("DropTCP: {:?}", e));
+        tcp.shutdown().await.unwrap_or_else(|_| {
+            println!("FATAL DropTcp");
+            std::process::exit(0);
+        });
 
         // Display/Store Results
         println!("HTTP: {}", String::from_utf8_lossy(&res));
